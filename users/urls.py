099d07297a8fe774d36364388eaf0352694d5538
Ermiyas Teklehaymanot <lilermi24@gmail.com>
2025-06-19 13:20:45 +0300

    feat: added password reset functionlaity to user login

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 
 urlpatterns = [
     path("register/", user_views.register, name='register'),
     path("login/", auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path("logout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path("user/profile/", user_views.profile, name='profile')
+    path("user/profile/", user_views.profile, name='profile'),
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset.html', html_email_template_name='users/password_reset_email.html'), name='password-reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
